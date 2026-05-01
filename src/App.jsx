60c8ffc041c606b5323cfd3bc4febdadf33a024d
saschadueser <saschadueser@gmail.com>
2026-05-01 16:25:41 +0200

    Added Hero module
    Added first data with the first Hero module

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 
 import Header from './Header.jsx';
+import Hero from './Hero.jsx';
 
 function App() {
 
   return (
     <>
       <Header />
+
+      <Hero />
     </>
   )
 }
---
+++ src/Hero.jsx
@@ -0,0 +1,11 @@
+import './styles/Hero.css'
+
+export default function Hero() {
+    return (
+        <div id="hero" className='hero-home' >
+            <h1>Headline</h1>
+            <p>Some Text</p>
+            <a href="" className="button">Click to do something</a>
+        </div>
+    )
+}
