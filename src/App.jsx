7dabcba2e7ba67e5b08f1210f518b1f4de16f15e
saschadueser <saschadueser@gmail.com>
2026-05-02 10:06:45 +0200

    Added service´s area as component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
-import './App.css'
+import './styles/App.css'
 
 import Header from './Header.jsx';
 import Hero from './Hero.jsx';
+import Leistungen from './Leistungen.jsx';
 
 function App() {
 
   return (
     <>
       <Header />
-
+      
       <Hero />
+
+    <main>
+        <Leistungen />
+    </main>
+
+    
     </>
   )
 }
---
+++ src/Leistungen.jsx
@@ -0,0 +1,25 @@
+import './styles/Leistungen.css'
+
+export default function Intro() {
+    return (
+        <section className="services">
+            <div className="service">
+                <h2>Website erstellen</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit mollitia accusantium ipsam dolores, eaque animi.</p>
+            </div>
+            <div className="service">
+                <h2>Updates & Wartung</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt iusto optio quibusdam nobis quam porro!</p>
+            </div>
+            <div className="service">
+                <h2>Optimierung der Website</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit mollitia accusantium ipsam dolores, eaque animi.</p>
+            </div>
+            <div className="service">
+                <h2>XXX</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt iusto optio quibusdam nobis quam porro!</p>
+            </div>
+        </section>
+
+    )
+}
